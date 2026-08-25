075e8c54d959c80b7c592c22014637a681b56ebc
Mohamed <ghalleb.mohamed2@gmail.com>
2026-08-25 10:53:09 +0200

    refactor: add HTML escaping boundary

---
+++ src/core/html.js
@@ -0,0 +1,13 @@
+/**
+ * Safe text helpers for the legacy renderer.
+ * Vue's template interpolation will provide equivalent escaping after migration.
+ */
+
+export function escapeHtml(value) {
+  return String(value ?? '')
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
